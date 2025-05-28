66fa2f4999043cf53e6cdc4b785344d6815f100e
CarlosMH-1212 <carlosmhinostroza@gmail.com>
2025-05-28 19:52:09 -0300

    arreglos 2

--- Refrigerado.php
+++ Refrigerado.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 require_once("Vehiculo.php");
 class Refrigerado extends Vehiculo{
@@ -6,13 +6,5 @@ class Refrigerado extends Vehiculo{
     return "Refirigerado realizando entrega productos climatizados";
     }
 }
-=======
-<?php
-require_once("Vehiculo.php");
-class Refrigerado extends Vehiculo{
-    public function RealizarEntrega(){    
-    return "Refirigerado realizando entrega productos climatizados";
-    }
-}
->>>>>>> 30ec33cf1e5539810cddd8bd8d8c0dd31906a5bd
+
 ?>
--- Utilitario.php
+++ Utilitario.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 require_once ("Vehiculo.php");
 class Utilitario extends Vehiculo {
@@ -6,13 +6,5 @@ class Utilitario extends Vehiculo {
     return "Utilitario realizando entrega rápida en zonas urbanas.";
     }
 }
-=======
-<?php
-require_once ("Vehiculo.php");
-class Utilitario extends Vehiculo {
-    public function RealizarEntrega(){    
-    return "Utilitario realizando entrega rápida en zonas urbanas.";
-    }
-}
->>>>>>> 30ec33cf1e5539810cddd8bd8d8c0dd31906a5bd
+
 ?>
