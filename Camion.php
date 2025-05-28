b6948cf5d668fe9bae4ee623c95ee69b8dda911e
CarlosMH-1212 <carlosmhinostroza@gmail.com>
2025-05-28 19:13:02 -0300

    subida 2

--- Camion.php
+++ Camion.php
@@ -1,7 +1,7 @@
 <?php
 class Camion extends Vehiculo {
     public function RealizarEntrega(){
-        return "Camión (ID: $this->id): Realizando entrega de carga pesada a largas distancias.";
+        return "Camión Realizando entrega de carga pesada a largas distancias.";
     }
 }
 a
