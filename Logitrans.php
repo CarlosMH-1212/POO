d77df60c0dcccb7d4a68bb5875620275f1f6715c
CarlosMH-1212 <carlosmhinostroza@gmail.com>
2025-06-07 17:58:36 -0300

    logitrans

--- Logitrans.php
+++ Logitrans.php
@@ -1,10 +1,15 @@
 <?php
 class Logitrans {
+    private $cant_flota_necesaria;
+    private $Tipo_vehiculo;
     
 
-
-
-
+     public function __construct ($cant_flota_necesaria,$Tipo_vehiculo){
+      
+       $this-> cant_flota_necesaria = $cant_flota_necesaria;
+       $this-> Tipo_vehiculo = $Tipo_vehiculo;
+ 
+    }
 
     }
 
